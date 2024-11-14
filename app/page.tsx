6f56952304d55fb208c8577b9297fbe033fb362d
SHORYA BAJ <jain.shorya11@gmail.com>
2024-11-14 12:40:31 +0530

    added acentricity ui lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,6 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import app from "@/config/firebase";
-import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const [user, setUser] = useState<User | null>(null);
-  const router = useRouter();
-  const auth = getAuth(app);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, setUser);
-    return unsubscribe; // Unsubscribe on component unmount
-  }, [auth]);
-
-  return (
-    <div>
-      {user ? (
-        <div>Hello, {user.email}</div>
-      ) : (
-        <>
-          <button onClick={() => router.push("/signup")}>Sign Up</button>
-          <button onClick={() => router.push("/signin")}>Sign In</button>
-        </>
-      )}
-    </div>
-  );
+  redirect("/dashboard");
+  return null;
 }
